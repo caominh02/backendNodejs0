4cc978b394db8c653edf5cf1d6bec71ab385c45e
TieuCa12 <caoducminh1412@gmail.com>
2023-11-11 01:06:30 +0700

    video 17

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+// khai bao route
 app.get('/', (req, res) => {
   res.send('Hello World with Minh!')
 })
+app.get('/viewPage', (req, res) => {
+  res.send('View Page with Minh!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
