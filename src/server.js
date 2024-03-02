5fd565e99e19ad9bb47b15c2bb367cd8d291bd24
TieuCa12 <caoducminh1412@gmail.com>
2024-03-02 14:39:10 +0700

    commit message done video 20

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const app = express()
-const port = 3000
-
 const path = require('path');
+require('dotenv').config();
+
+
+// IMPORT EXPRESS FROM 'EXPRESS'; // ES MODULE
+const app = express() // APP EXPRESS
+const port = process.env.PORT || 8888; // PORT => HARDCODE
+const hostname = process.env.HOST_NAME;
+
+
+
 // config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -10,7 +17,7 @@ app.set('view engine', 'ejs')
 // khai bao route
 // moi route tuong tu nhu 1 page
 app.get('/', (req, res) => {
-  res.send('Hello World with Minh!')
+  res.send('Hello World with Minh! test nodemon')
 })
 app.get('/viewPage', (req, res) => {
   // res.send('Check View Page with Minh!')
@@ -22,6 +29,6 @@ app.get('/testPage', (req, res) => {
   res.send('<h1 style="color:red;">Test page and css by NodeJs with Minh </h1>')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
